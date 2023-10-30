98e91aea7d8d5b78f029b2aa6a1e0fadc77edc6b
Gustavo Habib <46494399+gushabib@users.noreply.github.com>
2023-10-30 15:34:11 -0300

    Jogo em phyton

--- adivinhacao.py
+++ adivinhacao.py
@@ -6,12 +6,27 @@ print("*********************************")
 
 # numero_secreto = round(random.random() * 100)
 numero_secreto = random.randrange(1,101)
-total_de_tentativas = 3
-rodada = 1
+total_de_tentativas = 0
+pontos = 1000
+
+print("Qual nível de dificuldade deseja?")
+print("(1) - Fácil")
+print("(2) - Médio")
+print("(3) - Difícil")
+
+nivel = int(input("Digite o nível escolhido: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
 
 # while (rodada <= total_de_tentativas):
 for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+
     chute_str = input("Digite um número entre 1 e 100: ")
     print("Você digitou: ", chute_str)
     chute = int(chute_str)
@@ -25,13 +40,19 @@ for rodada in range(1, total_de_tentativas + 1):
     menor = chute < numero_secreto
 
     if (acertou):
-        print("Você acertou!")
+        print("Você acertou e fez {} pontos!".format(pontos))
         break
     else:
         if (maior):
             print("Você errou! O seu chute foi maior que o número secreto.")
+            if (rodada == total_de_tentativas):
+                print ("O número secreto era {}. Você fez {} pontos!".format(numero_secreto, pontos))
         elif (menor):
             print("Você errou! O seu chute foi menor que o número secreto.")
+            if (rodada == total_de_tentativas):
+                print ("O número secreto era {}. Você fez {} pontos!".format(numero_secreto, pontos))
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
     # rodada = rodada + 1
 
