e30c665b1d0df7baf07d7b9dfd72513ad2fe7eab
Gustavo Habib <46494399+gushabib@users.noreply.github.com>
2023-10-30 11:26:45 -0300

    Jogo em phyton

---
+++ adivinhacao.py
@@ -0,0 +1,38 @@
+import random
+
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+# numero_secreto = round(random.random() * 100)
+numero_secreto = random.randrange(1,101)
+total_de_tentativas = 3
+rodada = 1
+
+# while (rodada <= total_de_tentativas):
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute_str = input("Digite um número entre 1 e 100: ")
+    print("Você digitou: ", chute_str)
+    chute = int(chute_str)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
+
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if (acertou):
+        print("Você acertou!")
+        break
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    # rodada = rodada + 1
+
+print("Fim do jogo")
